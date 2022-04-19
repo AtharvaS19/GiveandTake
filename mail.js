644c82359d16acc798514a54efab3ef3005e91da
AtharvaS19 <93444391+AtharvaS19@users.noreply.github.com>
2022-04-19 17:20:10 +0530

    Update mail.js

--- mail.js
+++ mail.js
@@ -1,31 +1,20 @@
-// const firebaseConfig = {
-//     //   copy your firebase config informations
 
-//     apiKey: "AIzaSyA6EMj20K-1RRfnUpp-YZqMmaaqp_pRHCo",
-//     authDomain: "swipe-d3097.firebaseapp.com",
-//     databaseURL: "https://swipe-d3097-default-rtdb.firebaseio.com",
-//     projectId: "swipe-d3097",
-//     storageBucket: "swipe-d3097.appspot.com",
-//     messagingSenderId: "787064454414",
-//     appId: "1:787064454414:web:1fea024f06270c8d92ab5f",
-//     measurementId: "G-FV6TTNF4WS"
-// };
 
 const firebaseConfig = {
     apiKey: "AIzaSyDdRYz6gCSVqILZ2rAabe0gwfyHsSKaCZA",
     authDomain: "giventake-659fe.firebaseapp.com",
     projectId: "giventake-659fe",
-    databaseURL: "https://giventake-659fe-default-rtdb.firebaseio.com",
+    databaseURL: "https://giventake-65e-default-rtdb.firebaseio.com",
     storageBucket: "giventake-659fe.appspot.com",
     messagingSenderId: "1074629974109",
     appId: "1:1074629974109:web:c48d2452350e2746c695b4"
   };
 
 
-// initialize firebase
+
 firebase.initializeApp(firebaseConfig);
 
-// reference your database
+
 var contactFormDB = firebase.database().ref("contactForm");
 
 document.getElementById("contactForm").addEventListener("submit", submitForm);
